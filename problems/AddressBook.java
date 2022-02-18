07e15f1022b3456bcd857ecee6e47104caa29571
17harshu <harshalanandas17@gmail.com>
2022-02-18 20:11:13 +0530

    added: delete contact

--- problems/AddressBook.java
+++ problems/AddressBook.java
@@ -55,8 +55,8 @@ public class AddressBook {
 	private void getUserChoice() {
 		boolean isTerminate = false;
 		while (!isTerminate) {
-			System.out.println(
-					"1:Adding new person \n" + "2:Print address book \n" + "3:Update existing contact \n" + "4:Exit");
+			System.out.println("1:Adding new person \n" + "2:Print address book \n" + "3:Update the person details \n "
+					+ "4:Delete the person \n" + "5:Exit");
 			int option = scanner.nextInt();
 			switch (option) {
 			case 1:
@@ -73,6 +73,10 @@ public class AddressBook {
 				break;
 
 			case 4:
+				book.deletePerson();
+				break;
+
+			case 5:
 				isTerminate = true;
 				break;
 
@@ -137,5 +141,16 @@ public class AddressBook {
 			System.out.println("please select valid option");
 			break;
 		}
+	}
+
+	private void deletePerson() {
+		System.out.println("Enter EmailId to delete");
+		String EmailId = scanner.next();
+		if (!list.containsKey(EmailId)) {
+			System.out.println("Provided email Id is not found");
+			deletePerson();
+		}
+		list.remove(EmailId);
+		System.out.println("Deleted Successesfully.");
 	}
 }
